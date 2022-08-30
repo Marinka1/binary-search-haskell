3de923493d9d1ea097cd5683dcda92fc467063be
Marina <kompaniyetsmarina@gmail.com>
2022-08-29 21:10:19 -0400

    added binary search feature

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,20 @@
 module Main (main) where
-
 main :: IO ()
+
+binarysearch :: Int -> [Int] -> Int -> Int -> [Int]
+
+binarysearch _ _ l r
+    | l > r = []
+
+binarysearch x sortedList l r
+    | sortedList !! m == x = [m]
+    | sortedList !! m < x = binarysearch x sortedList (m+1) r
+    | otherwise = binarysearch x sortedList l (m-1)
+    where
+        m = ((r-l) `div` 2 + l)
+
+
+
+
 main = do
   putStrLn "hello world"
